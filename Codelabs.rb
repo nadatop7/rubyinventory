b3a5443f56be94e3a39f3b9555098f9e3d010254
Nathaniel Steed <nadatop2002@gmail.com>
2020-07-06 19:23:25 -0500

    commit

--- Codelabs.rb
+++ Codelabs.rb
@@ -1,2 +1,10 @@
-puts "Cheese = Time"
-puts "Power"
+puts "what is the year"
+year = gets
+puts "what is the make of the car"
+car = gets 
+puts "what car would you like"
+like = gets
+puts "what is your finance"
+finance = gets
+puts ""
+puts " #{year} #{car} #{like} #{finance}"
