ec00c140c48bfe9c63ff02bbee5a832169dd1531
Nathaniel Steed <nadatop2002@gmail.com>
2020-07-06 19:43:56 -0500

    commit

--- Codelabs.rb
+++ Codelabs.rb
@@ -2,10 +2,9 @@ puts "what is the year"
 year = gets
 puts "what is the make of the car"
 car = gets 
-puts "what car would you like"
+puts "Vehicle you would want"
 like = gets
 puts "what is your finance"
 finance = gets
 puts ""
-puts " #{year} #{car} #{like} #{finance}"
-puts "test"
+puts "You have said you have \n #{year} #{car} #{like} #{finance}"
