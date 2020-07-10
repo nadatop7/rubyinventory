5377d49987123bf389638b4b3147538e8ae32cbb
Nathaniel Steed <nadatop2002@gmail.com>
2020-07-09 19:13:51 -0500

    commit

--- Codelabs.rb
+++ Codelabs.rb
@@ -1,11 +1,30 @@
-puts "what is the year"
-year = gets.chomp
-puts "what is the make of the car"
-car = gets.chomp 
-puts "Vehicle you would want"
-like = gets.chomp
-puts "what is your finance"
-finance = gets.chomp
-puts ""
-puts "You have said you have \n #{year} #{car} #{like} #{finance}"
+def question(name)
+  puts "What is the #{name}"
+  input = gets.chomp
+  #puts "you put that you have #{input}"
+  input
+end
+
+def add_commas(num_string)
+    num_string.reverse.scan(/\d{3}|.+/).join(",").reverse 
+end
+  
+
+year = question("year")
+brand = question("brand")
+make = question("make")
+mileage = add_commas(question("mileage"))
+puts "#{year} #{brand} #{make} #{mileage}"
+
+
+#puts "what is the year"
+#year = gets.chomp
+#puts "what is the make of the car"
+#car = gets.chomp 
+#puts "Vehicle you would want"
+#like = gets.chomp
+#puts "what is your finance"
+#finance = gets.chomp
+#puts ""
+#puts "You have said you have \n #{year} #{brand} #{make} #{mileage}"
  
